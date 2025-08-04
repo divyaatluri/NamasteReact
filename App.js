b5e4df61ec4bc891f62b42f428f0b73cb1f2dbec
Divya Atluri <divy.atluri1992@gmail.com>
2025-08-04 16:53:21 +0800

    Created a food delivery application from scratch

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = [
+  React.createElement("h1", { className: "h1tag" }, "Hello World from React"),
+  React.createElement("h2", { className: "h2tag" }, "User Divya"),
+];
---
+++ index.js
@@ -0,0 +1,3 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
